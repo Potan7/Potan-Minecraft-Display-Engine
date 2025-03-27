172617abae9aa1be9d1ae6fda2fb21899d1ffe5e
Potan7 <whwo145@naver.com>
2025-03-28 01:07:10 +0900

    핫리로드 업데이트, Interpolation 시각화

--- Assets/Scripts/Animation/AnimFrame/Frame.cs
+++ Assets/Scripts/Animation/AnimFrame/Frame.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
-using System.Linq;
 using BDObjectSystem;
-using Manager;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using BDObjectSystem.Utility;
@@ -26,13 +24,14 @@ namespace Animation.AnimFrame
         public int interpolation;
         public string fileName;
         public TickLine tickLine;
+        public RectTransform interpolationRect;
 
         [Header("BDObject Info")]
         public BdObject Info;
         public Dictionary<string, BdObject> IDDataDict;
         public Dictionary<string, Matrix4x4> worldTransforms;
         private Timeline _timeline;
-        
+
 
         public void Init(string initFileName, int initTick, int inter, BdObject info, AnimObject obj, Timeline timeLine)
         {
@@ -40,10 +39,10 @@ namespace Animation.AnimFrame
             fileName = initFileName;
             animObject = obj;
             _initColor = outlineImage.color;
+            _timeline = timeLine;
             Info = info;
             tick = initTick;
             SetInter(inter);
-            _timeline = timeLine;
 
             UpdatePos();
             _timeline.OnGridChanged += UpdatePos;
@@ -58,7 +57,7 @@ namespace Animation.AnimFrame
             if (tick == 0)
                 return tick;
             if (!animObject.ChangePos(this, tick, newTick)) return tick;
-            
+
             tick = newTick;
             UpdatePos();
             return newTick;
@@ -81,13 +80,53 @@ namespace Animation.AnimFrame
                 rect.position = new Vector2(line.rect.position.x, rect.position.y);
                 tickLine = line;
             }
+            UpdateInterpolationBar();
         }
 
         public bool SetInter(int inter)
         {
+            if (interpolation < 0)
+                return false;
+
             interpolation = inter;
+            UpdateInterpolationBar();
+
             return true;
         }
+
+        private void UpdateInterpolationBar()
+        {
+            if (interpolation == 0)
+            {
+                interpolationRect.gameObject.SetActive(false);
+            }
+            else
+            {
+                interpolationRect.gameObject.SetActive(true);
+
+                var line = _timeline.GetTickLine(tick + interpolation, false);
+                line ??= _timeline.grid[_timeline.gridCount - 1];
+
+                // 1. line의 World Position 구함
+                Vector3 lineWorldPos = line.rect.position;
+
+                // 2. interpolationRect의 부모 기준 Local Position으로 변환
+                RectTransform parentRect = interpolationRect.parent as RectTransform;
+                RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRect,
+                    RectTransformUtility.WorldToScreenPoint(null, lineWorldPos),
+                    null,
+                    out Vector2 localPos);
+
+                // 3. interpolationRect 위치 조정 (왼쪽 고정이니까 anchoredPosition은 그대로)
+                float width = localPos.x - interpolationRect.anchoredPosition.x;
+                width += line.rect.rect.width / 2; // line의 rect width 반영
+
+                interpolationRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, width);
+            }
+        }
+
+
+
 
         public void OnPointerDown(PointerEventData eventData)
         {
@@ -105,7 +144,7 @@ namespace Animation.AnimFrame
         private void Update()
         {
             if (!isSelected) return;
-            
+
             Vector2 mouse = Input.mousePosition;
             var line = _timeline.GetTickLine(mouse);
 
@@ -117,7 +156,7 @@ namespace Animation.AnimFrame
                 outlineImage.color = _initColor;
             }
         }
-        
+
 
         public void RemoveFrame()
         {
@@ -125,6 +164,6 @@ namespace Animation.AnimFrame
             animObject.RemoveFrame(this);
         }
 
-    
+
     }
 }
